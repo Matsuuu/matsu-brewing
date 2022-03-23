0423c63629bdf09d45b009b100b188cd5ca4280e
Matsuuu <huhta.matias@gmail.com>
2022-03-23 12:05:00 +0200

    Finished css etiquette

--- logo-generator/components/etiquette-base/etiquette-base.js
+++ logo-generator/components/etiquette-base/etiquette-base.js
@@ -17,7 +17,7 @@ export class EtiquetteBase extends LitElement {
         super();
         this.scale = 1;
         this.splashImage = html``;
-        this.splashImageFontSize = "3.5px";
+        this.splashImageFontSize = "1.75px";
         this.beerName = "";
         this.info = {};
     }
@@ -39,16 +39,26 @@ export class EtiquetteBase extends LitElement {
 
     render() {
         return html`
-      <div class="splash-image">${this.splashImage}</div>
-      <div class="neofetch">
-        <p class="name">${this.beerName}@Matsu-Brewing</p>
-        <p>${"-".repeat(`${this.beerName}@Matsu-Brewing`.length)}</p>
-        <br />
-        ${Object.keys(this.info).map(
+      <div class="main-label">
+        <div class="splash-image">${this.splashImage}</div>
+        <div class="neofetch">
+          <p class="name">${this.beerName} @ Matsu-Brewing</p>
+          <p>${"-".repeat(`${this.beerName}@Matsu-Brewing`.length)}</p>
+          ${Object.keys(this.info).map(
             (key) => html` <p><b>${key}</b>: ${this.info[key]}</p> `
         )}
+        </div>
       </div>
-      <main-logo scale="0.5" background-color="transparent"></main-logo>
+      <div class="terminal-line">
+        <span class="username">matsu</span>
+        <span class="directory">~/beers/developer-series</span>
+        <span class="caret"> > </span>
+        <div class="blinker"></div>
+      </div>
+      <main-logo
+        scale="${this.scale * 0.25}"
+        background-color="transparent"
+      ></main-logo>
     `;
     }
 
@@ -57,14 +67,15 @@ export class EtiquetteBase extends LitElement {
             css`
         :host {
           --scale: 1;
-          --splash-image-font-size: 3.5px;
+          --splash-image-font-size: 1.75px;
 
           position: relative;
           box-shadow: 0 10px 16px 0 rgb(0 0 0 / 20%),
             0 6px 20px 0 rgb(0 0 0 / 19%);
-          width: calc(500px * var(--scale));
-          height: calc(200px * var(--scale));
+          width: calc(520px * var(--scale));
+          height: calc(220px * var(--scale));
           display: flex;
+          flex-direction: column;
           margin: 1rem;
           align-items: center;
           padding: 3rem 3rem;
@@ -72,24 +83,28 @@ export class EtiquetteBase extends LitElement {
 
           background: var(--background-color, #202330);
         }
+        .main-label {
+          display: flex;
+        }
 
         .splash-image {
-          font-size: var(--splash-image-font-size);
+          font-size: calc(var(--splash-image-font-size) * var(--scale));
           margin-right: 4rem;
         }
 
         .neofetch {
           display: flex;
           height: 100%;
-          width: 100%;
+          width: 50%;
           flex-direction: column;
           color: var(--secondary-color);
+          font-size: calc(var(--scale) * 0.55rem);
         }
 
         .neofetch > p {
           padding: 0;
-          margin: 0;
-          line-height: 1.2;
+          margin: 0 0 6px 0;
+          line-height: calc(var(--scale) * 0.55rem);
         }
 
         .neofetch b {
@@ -98,6 +113,7 @@ export class EtiquetteBase extends LitElement {
 
         .name {
           color: var(--main-color);
+            font-weight: bold;
         }
 
         .brewery-logo {
@@ -106,13 +122,36 @@ export class EtiquetteBase extends LitElement {
         main-logo {
           position: absolute;
           right: -4rem;
-          top: auto;
-          bottom: auto;
+          top: 0;
+          bottom: 0;
+          margin: auto;
           transform: rotate(-90deg);
 
           --text-color: var(--logo-text-color);
           --text-alt-color: var(--logo-text-alt-color);
           --background-color: var(--logo-background-color);
+        }
+
+        .terminal-line {
+          width: 100%;
+          padding: 1rem 0 1rem 0.5rem;
+          display: flex;
+          gap: 0.5rem;
+        }
+
+        .username,
+        .caret {
+          color: var(--main-color);
+        }
+
+        .directory {
+          color: var(--secondary-color);
+        }
+
+        .blinker {
+          background: var(--main-color);
+          width: calc(var(--scale) * 0.275rem);
+          height: calc(var(--scale) * 0.55rem);
         }
       `,
         ];
